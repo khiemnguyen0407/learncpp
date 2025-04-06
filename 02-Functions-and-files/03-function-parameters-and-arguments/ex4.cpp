7341841ea381bc6ba3229b59733781e2ee7ff3a1
Coffee Milk <herokhiem@yahoo.com>
2025-04-06 21:07:30 +0100

    ex4.cpp

--- 02-Functions-and-files/03-function-parameters-and-arguments/ex4.cpp
+++ 02-Functions-and-files/03-function-parameters-and-arguments/ex4.cpp
@@ -14,10 +14,23 @@ int add(int x, int y)
     return x + y;
 }
 
+int multiply(int x, int y)
+{
+    return x * y;
+}
+
 // main takes no parameters
 int main()
 {
     std::cout << add(4, 5) << '\n'; // Arguments 4 and 5 are passed to function add()
+
+    std::cout << add(1 + 2, 3 * 4) << '\n'; // within add() x=3, y=12, so x+y=15
+
+    int a{ 5 };
+    std::cout << add(a, a) << '\n'; // evaluates (5 + 5)
+
+    std::cout << add(1, multiply(2, 3)) << '\n'; // evaluates 1 + (2 * 3)
+    std::cout << add(1, add(2, 3)) << '\n'; // evaluates 1 + (2 + 3)
     return 0;
 }
 
