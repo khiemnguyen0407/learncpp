25a0823e443a5b6f33b88ddec3d14bc991a9cd59
Coffee Milk <herokhiem@yahoo.com>
2025-04-08 01:33:08 +0100

    update

--- 02-Functions-and-files/05-forward-declarations-and-definitions/ex1.cpp
+++ 02-Functions-and-files/05-forward-declarations-and-definitions/ex1.cpp
@@ -1,8 +1,13 @@
 /* Option 1: Reorder the function definitions */
 
-/* 
-One way to address the issue is to reorder the function definitions so add is defined before main:
+/*
+One way to address the issue is to reorder the function definitions so add is defined before main. 
+That way, by the time main calls add, the compiler will already know what add is. Because this is 
+such a simple program, this change is relatively easy to do. However, in a larger program, it can 
+be tedious trying to figure out which functions call which other functions (and in what order) so 
+they can be declared sequentially.
 */
+
 #include <iostream>
 
 int add(int x, int y)
@@ -15,15 +20,3 @@ int main()
     std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
     return 0;
 }
-/*
-That way, by the time main calls add, the compiler will already know what add is. Because this is 
-such a simple program, this change is relatively easy to do. However, in a larger program, it can 
-be tedious trying to figure out which functions call which other functions (and in what order) so 
-they can be declared sequentially.
-
-Furthermore, this option is not always possible. Let's say we are writing a program that has two 
-functions A and B. If function A calls function B, and function B calls function A, then there is 
-no way to order the functions in a way that will make the compiler happy. If you define A first, 
-the compiler will complain it doesn’t know what B is. If you define B first, the compiler will 
-complain that it doesn’t know what A is.
-*/
