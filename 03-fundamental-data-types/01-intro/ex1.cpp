5c99c3d650d0fbe58bf432562cdd03c7fb9d7ec5
cafesua <herokhiem@yahoo.com>
2025-04-11 20:42:49 +0100

    update

---
+++ 03-fundamental-data-types/01-intro/ex1.cpp
@@ -0,0 +1,30 @@
+/* Fundamental data types */
+
+/*
+The C++ language comes with many predefined data types available for your use. The most basic of 
+these types are called the fundamental data types (informally sometimes called basic types or 
+primitive types).
+
+Here is a list of the fundamental data types, some of which you have already seen:
+
+Types	                Category	            Meaning	                                            Example
+float                   Floating Point	        a number with a fractional part	                    3.14
+double
+long double	
+bool	                Integral (Boolean)	    true or false	                                    true
+char                    Integral (Character)    a single character of text	                        'c'
+wchar_t
+char8_t (C++20)
+char16_t (C++11)
+char32_t (C++11)		
+short int               Integral (Integer)      positive and negative whole numbers, including 0    64
+int
+long int
+long long int (C++11)	
+std::nullptr_t (C++11)	Null Pointer	        a null pointer	                                    nullptr
+void	                Void	                no type	n/a
+*/
+
+/*
+Integer vs integral types
+*/
