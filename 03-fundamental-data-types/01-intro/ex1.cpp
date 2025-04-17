90e96c800043733ec593f8a681cb040b2983b3b9
cafesua <herokhiem@yahoo.com>
2025-04-17 18:47:18 +0100

    update 03-folder

--- 03-fundamental-data-types/01-intro/ex1.cpp
+++ 03-fundamental-data-types/01-intro/ex1.cpp
@@ -28,3 +28,34 @@ void	                Void	                no type	n/a
 /*
 Integer vs integral types
 */
+
+#include <iostream>
+int main()
+{
+    // floating point number -- real number in mathematics
+    float x1 { 3.14 };
+    double x2 { 3.1415 };
+    long double x3 { 3.1415 };
+
+    // boolean variable storing value "true" or "false"
+    bool b1 { true };
+    bool b2 { false };
+    
+    // Character. You do not need to worry too much about other complex char-type data types.
+    char c { 'a' };
+    
+    // Integer: You probably mostly use the data type "int"
+    int i2 { 24 };          
+    short int i1 { 42 };    // smaller size than int
+
+    // We talk about null pointer later.
+    
+    std::cout << "x1 = " << x1 << "\nx2 = " << x2 << "\nx3 = " << x3 << '\n';
+
+    // The boolean "true" is represented as "1", "false" as "0"
+    std::cout << "b1 = " << b1 << "\nb2 = " << b2 << '\n';
+
+    // To print out "true" or "false", we must use std::boolalpha
+    std::cout << "b1 = " << std::boolalpha << b1 << "\nb2 = " << std::boolalpha << b2 << '\n'; 
+    return 0;
+}
