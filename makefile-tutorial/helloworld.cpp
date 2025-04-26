08d6e9a2ad5ae58912fa50307df5a1b8ffd1adb2
cafesua <herokhiem@yahoo.com>
2025-04-26 16:04:30 +0100

    makefile tutorial

---
+++ makefile-tutorial/helloworld.cpp
@@ -0,0 +1,8 @@
+// helloworld.cpp
+#include <iostream>
+int main()
+{
+    std::cout << "Just wanna say hello the world!\n";
+    std::cout << "I am Khiem, your instructor of this year.";
+    return 0;
+}
